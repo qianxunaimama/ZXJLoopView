140be220ff8fc264b3e3224c51d9698dad055a28
张信娟 <zhangxinjuan@promote.cache-dns.local>
2016-07-22 22:08:04 +0800

    commit:初始化提交。

---
+++ ZXJLoopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXJLoopView
+//
+//  Created by 张信娟 on 16/7/22.
+//  Copyright © 2016年 张信娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXJLoopView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXJLoopView
+//
+//  Created by 张信娟 on 16/7/22.
+//  Copyright © 2016年 张信娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
